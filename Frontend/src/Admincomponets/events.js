203d129d809e51da5e73672d6984a48d7cfb3e0a
TrialBaseCode <compwork321@gmail.com>
2023-06-13 17:50:01 +0530

    fleet manage 13 june 2023

--- Frontend/src/Admincomponets/events.js
+++ Frontend/src/Admincomponets/events.js
@@ -53,16 +53,42 @@ const Events = () => {
                       
                         {/* <!------Content work---------->*/}
                          <div className="events_nav_list">
-                            <nav class="list-group list-group-horizontal mytabtlist" >
-                                <Link class="list-group-item active text-decoration-none"  data-toggle="list"  to="/events/allevents" ><span>ALL EVENTS</span></Link>
-                                <Link class="list-group-item text-decoration-none" data-toggle="list"   to="/events/feaevents"><span>FEATURE EVENTS</span></Link>
+                            <nav className="list-group list-group-horizontal mytabtlist" >
+                                <Link className="list-group-item active text-decoration-none"  data-toggle="list"  to="/events/allevents" ><span>ALL EVENTS</span></Link>
+                                <Link className="list-group-item text-decoration-none" data-toggle="list"   to="/events/feaevents"><span>FEATURE EVENTS</span></Link>
                             </nav>
-                      
                         </div> 
-                 
-
-                         <div className="allcontainer">
-                            <Outlet />
+                         <div className="alleventcontainer pl-3 pr-3">
+                            <div className="filter-work-box">
+                                <div className="filter d-flex justify-content-between">
+                                    <div className="filter-drop  d-flex">
+                                        <div class="dropdown">
+                                        <button class="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <span className="pr-5">All Events</span>   
+                                        </button>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                            <a class="dropdown-item" href="#">Something else here</a>
+                                        </div>
+                                        </div>
+                                        <div class="dropdown">
+                                        <button class="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            <span className="pr-5">All Vehicles</span>   
+                                        </button>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                            <a class="dropdown-item" href="#">Action</a>
+                                            <a class="dropdown-item" href="#">Another action</a>
+                                            <a class="dropdown-item" href="#">Something else here</a>
+                                        </div>
+                                        </div>
+                                    </div>
+                                    <button type="button" class="btn btn-outline-danger">Clear</button>
+                                </div>
+                            </div>
+                            <div className="listContainbox">
+                              <Outlet />
+                            </div>
                          </div>
 
 
--- Frontend/src/Admincomponets/events/Feaevents.js
+++ Frontend/src/Admincomponets/events/Feaevents.js
@@ -1,7 +1,7 @@
 const Feaevents = () => {
     return ( 
         <>
-            <div className="feaEventsContainer">
+              <div className="AllEventsContainer">
                 <div className="container p-0">
                        {/*  <!-- Content table Row --> */}
 
@@ -9,32 +9,91 @@ const Feaevents = () => {
 
                             {/*   <!-- Area Chart --> */}
                             <div className="col-xl-12 col-lg-12">
-                                <div className="card shadow mb-4">
-                                    {/*  <!-- Card Header - Dropdown --> */}
-                                    <div
-                                        className="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                        <h6 className="m-0 font-weight-bold text-primary">Feature Events</h6>
-                                        <div className="dropdown no-arrow">
-                                            <a className="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                <i className="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-                                            </a>
-                                            <div className="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-                                                aria-labelledby="dropdownMenuLink">
-                                                <div className="dropdown-header">Dropdown Header:</div>
-                                                <a className="dropdown-item" href="#">Action</a>
-                                                <a className="dropdown-item" href="#">Another action</a>
-                                                <div className="dropdown-divider"></div>
-                                                <a className="dropdown-item" href="#">Something else here</a>
+                                <div className="event-cart-container">
+                                    {/* ---Start events card header--- */}
+                                    <div className="card newEventListcard shadow mb-4">
+                                        {/*  <!-- Card Header - Dropdown --> */}
+                                        <div className="card-header py-3 d-flex flex-row align-items-center bg-primary">
+                                            <div className="select-all">
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="checkbox" value=""/>
+                                                </div>
                                             </div>
+                                            <h6 className="m-0 font-weight-bold text-white listEventType">EVENT TYPE</h6>
+                                            <h6 className="m-0 font-weight-bold text-white  listEventType">MESSAGE</h6>
+                                            <h6 className="m-0 font-weight-bold text-white  listEventType">DATE&TIME</h6>
+                                            <h6 className="m-0 font-weight-bold text-white  listEventType">VEHICLE</h6>
+                                            <div class="dropdown no-arrow"><a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i></a><div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink"><div class="dropdown-header">Dropdown Header:</div><a class="dropdown-item" href="#">Action</a><a class="dropdown-item" href="#">Another action</a><div class="dropdown-divider"></div><a class="dropdown-item" href="#">Something else here</a></div></div>
+                                        </div>
+
+                                    </div>
+                                    {/* ---End events card header--- */}
+                                    {/* ---Start events card no of list list--- */}
+                                    <div className="list-of-eventsCarts">
+                                        <div className="card newEventListcard shadow mb-4">
+                                            {/*  <!-- Card Header - Dropdown --> */}
+                                            <div className="card-header card-responsive py-3  flex-row align-items-center bg-primary">
+                                                <div className="select-all">
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="checkbox" value=""/>
+                                                    </div>
+                                                </div>
+                                                <h6 className="m-0 font-weight-bold text-white listEventType">EVENT TYPE</h6>
+                                                <h6 className="m-0 font-weight-bold text-white  listEventType">MESSAGE</h6>
+                                                <h6 className="m-0 font-weight-bold text-white  listEventType">DATE&TIME</h6>
+                                                <h6 className="m-0 font-weight-bold text-white  listEventType">VEHICLE</h6>
+                                                <div class="dropdown no-arrow"><a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i></a><div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink"><div class="dropdown-header">Dropdown Header:</div><a class="dropdown-item" href="#">Action</a><a class="dropdown-item" href="#">Another action</a><div class="dropdown-divider"></div><a class="dropdown-item" href="#">Something else here</a></div></div>
+                                            </div>
+                                            {/*  <!-- Card Body --> */}
+                                            <div className="card-body ">
+                                                <div className="indexList py-3 d-flex flex-row align-items-center ">
+                                                    <div className="select">
+                                                        <div class="form-check">
+                                                            <input class="form-check-input" type="checkbox" value=""/>
+                                                        </div>
+                                                    </div>
+                                                    <h6 className="m-0  text-primary listbdy">Success</h6>
+                                                    <h6 className="m-0  text-primary listbdy">Maintenance passed</h6>
+                                                    <h6 className="m-0  text-primary listbdy">5/13/2023 2:54PM</h6>
+                                                    <h6 className="m-0  text-primary listbdy">AB 2515</h6>
+                                                    <div class="dropdown no-arrow"><a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i></a><div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink"><div class="dropdown-header">Dropdown Header:</div><a class="dropdown-item" href="#">Action</a><a class="dropdown-item" href="#">Another action</a><div class="dropdown-divider"></div><a class="dropdown-item" href="#">Something else here</a></div></div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <div className="card newEventListcard shadow mb-4">
+                                            {/*  <!-- Card Header - Dropdown --> */}
+                                            <div className="card-header card-responsive py-3  flex-row align-items-center bg-primary">
+                                                <div className="select-all">
+                                                    <div class="form-check">
+                                                        <input class="form-check-input" type="checkbox" value=""/>
+                                                    </div>
+                                                </div>
+                                                <h6 className="m-0 font-weight-bold text-white listEventType">EVENT TYPE</h6>
+                                                <h6 className="m-0 font-weight-bold text-white  listEventType">MESSAGE</h6>
+                                                <h6 className="m-0 font-weight-bold text-white  listEventType">DATE&TIME</h6>
+                                                <h6 className="m-0 font-weight-bold text-white  listEventType">VEHICLE</h6>
+                                                <div class="dropdown no-arrow"><a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i></a><div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink"><div class="dropdown-header">Dropdown Header:</div><a class="dropdown-item" href="#">Action</a><a class="dropdown-item" href="#">Another action</a><div class="dropdown-divider"></div><a class="dropdown-item" href="#">Something else here</a></div></div>
+                                            </div>
+                                            {/*  <!-- Card Body --> */}
+                                            <div className="card-body ">
+                                                <div className="indexList py-3 d-flex flex-row align-items-center ">
+                                                    <div className="select">
+                                                        <div class="form-check">
+                                                            <input class="form-check-input" type="checkbox" value=""/>
+                                                        </div>
+                                                    </div>
+                                                    <h6 className="m-0  text-primary listbdy">Success</h6>
+                                                    <h6 className="m-0  text-primary listbdy">Maintenance passed</h6>
+                                                    <h6 className="m-0  text-primary listbdy">5/13/2023 2:54PM</h6>
+                                                    <h6 className="m-0  text-primary listbdy">AB 2515</h6>
+                                                    <div class="dropdown no-arrow"><a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i></a><div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink"><div class="dropdown-header">Dropdown Header:</div><a class="dropdown-item" href="#">Action</a><a class="dropdown-item" href="#">Another action</a><div class="dropdown-divider"></div><a class="dropdown-item" href="#">Something else here</a></div></div>
+                                                </div>
+                                            </div>
+
                                         </div>
                                     </div>
-                                    {/*  <!-- Card Body --> */}
-                                    <div className="card-body">
-                                        <div className="chart-area">
-                                            <canvas id="myAreaChart"></canvas>
-                                        </div>
-                                    </div>
+                                    {/* ---end events card no of list list--- */}
                                 </div>
                             </div>
 
