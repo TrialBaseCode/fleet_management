922661647445b17ea5af8cd3caf67b934f29302d
TrialBaseCode <compwork321@gmail.com>
2023-06-19 10:35:20 +0530

    fleet management 19/6/23

--- Frontend/public/js/sb-newadmin.js
+++ Frontend/public/js/sb-newadmin.js
@@ -1,20 +1,25 @@
 $(document).ready(function(){
-    $('.sidebarwork .nav-item').click(function(){
-      $('.sidebarwork .nav-item').removeClass("active");
+    $('#fleetAdminal li' ).click(function(){
+      $('#fleetAdminal li ').removeClass("active");
       $(this).addClass("active");
+    });
+});
+
+$(document).ready(function() {
+    // Get saved data from sessionStorage
+    let selectedCollapse = sessionStorage.getItem('selectedCollapse');
+    if(selectedCollapse != null) {
+      $('.accordion .collapse').removeClass('show');
+      $(selectedCollapse).addClass('show');
+    }
+    // //To set, which one will be opened
+    // $('.accordion .btn-link').on('click', function(){ 
+    //   let target = $(this).data('target');
+    //   //Save data to sessionStorage
+    //   sessionStorage.setItem('selectedCollapse', target);
+    // });
+    $('#accordionSidebar' ).click(function(){
+        let target = $(this).data('target');
+        sessionStorage.setItem('selectedCollapse', target);
+    });
   });
-
-
-
-$('a[data-toggle="tab"]').on('show.bs.tab', function(e) {
-    localStorage.setItem('activeTab', $(e.target).attr('href'));
-});
-
-var activeTab = localStorage.getItem('activeTab');
-
-console.log(activeTab);
-
-if (activeTab) {
-   $('a[href="' + activeTab + '"]').tab('show');
-}
-});
--- Frontend/src/Admincomponets/Sidebar.js
+++ Frontend/src/Admincomponets/Sidebar.js
@@ -29,10 +29,10 @@ function Sidebar() {
           <>
                 <div className={style}>
                     <nav className="sidebarwork" id="accordionSidebar">
-                        <Link className="nav-item" to="/test">Test</Link >
-                        <Link  className="nav-item" to="/apitest">ApiTest</Link >
+                        <Link className="collapse-item" to="/test">Test</Link>
+                        <Link className="collapse-item" to="/apitest">ApiTest</Link>
                         {/*  <!-- Sidebar - Brand --> */}
-                        <Link  className=" nav-item sidebar-brand d-flex align-items-center justify-content-center" to="/">
+                        <Link className="sidebar-brand d-flex align-items-center justify-content-center" to="/">
                             <div className="sidebar-brand-icon rotate-n-15">
                             <i className="fas fa-truck"></i>
                             </div>
@@ -44,150 +44,151 @@ function Sidebar() {
                         
                         {/* <!---Test Case ---> */}
                 
-                       
-                        {/*  <!-- Divider --> */}
-                        <hr className="sidebar-divider" />
+                        {/*   <!-- Divider --> */}
+                        <hr className="sidebar-divider my-0" />
+                        <ul className='fleetAdminDetail p-0' id='fleetAdminal'>
                         
-                        {/*  <!-- Nav Item - Dashboard --> */}
-                        <Link className="nav-item active">
-                            <div className="nav-link"  data-toggle="collapse" data-target="#collapsedash"
-                                aria-expanded="true" aria-controls="collapsedash">
-                                <i className="fas fa-fw fa-tachometer-alt"></i>
-                                <span>Dashboard</span>
-                            </div>
-                            <div id="collapsedash" className="collapse show" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                                <div className="bg-white py-2 collapse-inner rounded">
-                                    <NavLink className="collapse-item"  to="/">Vehicles Overview</NavLink>
-                                    <NavLink className="collapse-item" to="/realTimeData">Real Time Data</NavLink>
-                                    <NavLink className="collapse-item" to="/events">Events</NavLink>
-                                </div>
-                            </div>
-                        </Link>
+                                {/*  <!-- Nav Item - Dashboard --> */}
+                                <li className="nav-item active">
+                                    <a className="nav-link"  data-toggle="collapse" data-target="#collapsedash"
+                                        aria-expanded="true" aria-controls="collapsedash">
+                                        <i className="fas fa-fw fa-tachometer-alt"></i>
+                                        <span>Dashboard</span>
+                                    </a>
+                                    <div id="collapsedash" className="collapse show" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                                        <div className="bg-white py-2 collapse-inner rounded">
+                                            <NavLink className="collapse-item"  to="/">Vehicles Overview</NavLink>
+                                            <NavLink className="collapse-item" to="/realTimeData">Real Time Data</NavLink>
+                                            <NavLink className="collapse-item" to="/events">Events</NavLink>
+                                        </div>
+                                    </div>
+                                </li>
 
-                        {/*  <!-- Divider --> */}
-                        <hr className="sidebar-divider" />
-
-
-                        {/*  <!-- Nav Item - Pages Collapse Menu --> */}
-                        <Link className="nav-item">
-                            <div className="nav-link collapsed "  data-toggle="collapse" data-target="#collapseTwo"
-                                aria-expanded="true" aria-controls="collapseTwo">
-                                <i className="fas fa-car"></i>
-                                <span>Vehicles</span>
-                            </div>
-                            <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                                <div className="bg-white py-2 collapse-inner rounded">
-                                    <NavLink className="collapse-item" to="/vifleet">Fleet</NavLink>
-                                    <NavLink className="collapse-item" to="/vehiclesassign">Vehicles Assignment</NavLink>
-                                    <NavLink className="collapse-item" to="/Vehiclesdetails">Vehicle Details</NavLink>
-                                </div>
-                            </div>
-                        </Link>
+                                {/*  <!-- Divider --> */}
+                                <hr className="sidebar-divider" />
 
 
-
-                        {/* <!-- Nav Item - Utilities Collapse Menu --> */}
-                        <Link className="nav-item">
-                            <div className="nav-link collapsed"  data-toggle="collapse" data-target="#collapseUtilities"
-                                aria-expanded="true" aria-controls="collapseUtilities">
-                            <i className="fas fa-suitcase-rolling"></i>
-                                <span>Trips</span>
-                            </div>
-                            <div id="collapseUtilities" className="collapse" aria-labelledby="headingUtilities"
-                                data-parent="#accordionSidebar">
-                                <div className="bg-white py-2 collapse-inner rounded">
-                                    <NavLink className="collapse-item" to="utilities-color.html">Overview</NavLink>
-                                    <NavLink className="collapse-item" to="utilits-border.html">Upcoming Trip</NavLink>
-                                    <NavLink className="collapse-item" to="utis-animation.html">Schednave Trip</NavLink>
-                                </div>
-                            </div>
-                        </Link>
-
-                        {/*  <!-- Divider --> */}
-                        <hr className="sidebar-divider" />
-
-                        {/* <!-- Nav Item - Utilities Collapse Menu --> */}
-                        <Link className="nav-item">
-                            <div className="nav-link collapsed"  data-toggle="collapse" data-target="#collapseMainten"
-                                aria-expanded="true" aria-controls="collapseMainten">
-                            <i className="fas fa-tools"></i>
-                                <span>Maintenance</span>
-                            </div>
-                            <div id="collapseMainten" className="collapse" aria-labelledby="headingMainten"
-                                data-parent="#accordionSidebar">
-                                <div className="bg-white py-2 collapse-inner rounded">
-                                    <NavLink className="collapse-item" to="utilities-color.html">Maintenance Planner</NavLink>
-                                </div>
-                            </div>
-                        </Link>
+                                {/*  <!-- Nav Item - Pages Collapse Menu --> */}
+                                <li className="nav-item">
+                                    <a className="nav-link collapsed "  data-toggle="collapse" data-target="#collapseTwo"
+                                        aria-expanded="true" aria-controls="collapseTwo">
+                                        <i className="fas fa-car"></i>
+                                        <span>Vehicles</span>
+                                    </a>
+                                    <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                                        <div className="bg-white py-2 collapse-inner rounded">
+                                            <NavLink className="collapse-item" to="/vifleet">Fleet</NavLink>
+                                            <NavLink className="collapse-item" to="/vehiclesassign">Vehicles Assignment</NavLink>
+                                            <NavLink className="collapse-item" to="/Vehiclesdetails">Vehicle Details</NavLink>
+                                        </div>
+                                    </div>
+                                </li>
 
 
-                        {/* <!-- Nav Item - history --> */}
-                        <Link className="nav-item">
-                            <NavLink className="nav-link" to="charts.html">
-                                <i className="fas fa-history"></i>
-                                <span>Hisory</span></NavLink>
-                        </Link>
-                        {/*  <!-- Divider --> */}
-                        <hr className="sidebar-divider" />
 
-                        {/* <!-- Nav Item - history --> */}
-                        <Link className="nav-item">
-                            <a className="nav-link" href="charts.html">
-                                <i className="fas fa-fw fa-chart-area"></i>
-                                <span>Analytics</span></a>
-                        </Link>
+                                {/* <!-- Nav Item - Utilities Collapse Menu --> */}
+                                <li className="nav-item" >
+                                    <div className="nav-link collapsed"  data-toggle="collapse" data-target="#collapseUtilities"
+                                        aria-expanded="true" aria-controls="collapseUtilities">
+                                    <i className="fas fa-suitcase-rolling"></i>
+                                        <span>Trips</span>
+                                    </div>
+                                    <div id="collapseUtilities" className="collapse" aria-labelledby="headingUtilities"
+                                        data-parent="#accordionSidebar">
+                                        <div className="bg-white py-2 collapse-inner rounded">
+                                            <NavLink className="collapse-item" to="utilities-color.html">Overview</NavLink>
+                                            <NavLink className="collapse-item" to="utilits-border.html">Upcoming Trip</NavLink>
+                                            <NavLink className="collapse-item" to="utis-animation.html">Schednave Trip</NavLink>
+                                        </div>
+                                    </div>
+                                </li>
 
-                                    
-                        {/*  <!-- Divider --> */}
-                        <hr className="sidebar-divider" />
+                                {/*  <!-- Divider --> */}
+                                <hr className="sidebar-divider" />
+
+                                {/* <!-- Nav Item - Utilities Collapse Menu --> */}
+                                <li className="nav-item">
+                                    <div className="nav-link collapsed"  data-toggle="collapse" data-target="#collapseMainten"
+                                        aria-expanded="true" aria-controls="collapseMainten">
+                                    <i className="fas fa-tools"></i>
+                                        <span>Maintenance</span>
+                                    </div>
+                                    <div id="collapseMainten" className="collapse" aria-labelledby="headingMainten"
+                                        data-parent="#accordionSidebar">
+                                        <div className="bg-white py-2 collapse-inner rounded">
+                                            <NavLink className="collapse-item" to="utilities-color.html">Maintenance Planner</NavLink>
+                                        </div>
+                                    </div>
+                                </li>
 
 
-                        {/*  <!-- Nav Item - Pages Collapse Menu --> */}
-                        <Link className="nav-item">
-                            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-                                aria-expanded="true" aria-controls="collapsePages">
-                                <i className="fas fa-fw fa-folder"></i>
-                                <span>Pages</span>
-                            </a>
-                            <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                                <div className="bg-white py-2 collapse-inner rounded">
-                                    <h6 className="collapse-header">Login Screens:</h6>
-                                    <a className="collapse-item" href="login.html">Login</a>
-                                    <a className="collapse-item" href="register.html">Register</a>
-                                    <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
-                                    <div className="collapse-divider"></div>
-                                    <h6 className="collapse-header">Other Pages:</h6>
-                                    <a className="collapse-item" href="404.html">404 Page</a>
-                                    <a className="collapse-item" href="blank.html">Blank Page</a>
-                                </div>
-                            </div>
-                        </Link>
+                                {/* <!-- Nav Item - history --> */}
+                                <li className="nav-item">
+                                    <NavLink className="nav-link" to="charts.html">
+                                        <i className="fas fa-history"></i>
+                                        <span>Hisory</span></NavLink>
+                                </li>
+                                {/*  <!-- Divider --> */}
+                                <hr className="sidebar-divider" />
 
-                        {/* <!-- Nav Item - Charts --> */}
-                        <Link className="nav-item">
-                            <a className="nav-link" href="charts.html">
-                                <i className="fas fa-fw fa-chart-area"></i>
-                                <span>Charts</span></a>
-                        </Link>
+                                {/* <!-- Nav Item - history --> */}
+                                <li className="nav-item">
+                                    <a className="nav-link" href="charts.html">
+                                        <i className="fas fa-fw fa-chart-area"></i>
+                                        <span>Analytics</span></a>
+                                </li>
 
-                        {/*  <!-- Nav Item - Tables --> */}
-                        <Link className="nav-item">
-                            <a className="nav-link" href="tables.html">
-                                <i className="fas fa-fw fa-table"></i>
-                                <span>Tables</span></a>
-                        </Link>
+                                            
+                                {/*  <!-- Divider --> */}
+                                <hr className="sidebar-divider" />
 
-                        {/* <!-- Divider --> */}
-                        <hr className="sidebar-divider d-none d-md-block" />
 
+                                {/*  <!-- Nav Item - Pages Collapse Menu --> */}
+                                <li className="nav-item">
+                                    <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+                                        aria-expanded="true" aria-controls="collapsePages">
+                                        <i className="fas fa-fw fa-folder"></i>
+                                        <span>Pages</span>
+                                    </a>
+                                    <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+                                        <div className="bg-white py-2 collapse-inner rounded">
+                                            <h6 className="collapse-header">Login Screens:</h6>
+                                            <a className="collapse-item" href="login.html">Login</a>
+                                            <a className="collapse-item" href="register.html">Register</a>
+                                            <a className="collapse-item" href="forgot-password.html">Forgot Password</a>
+                                            <div className="collapse-divider"></div>
+                                            <h6 className="collapse-header">Other Pages:</h6>
+                                            <a className="collapse-item" href="404.html">404 Page</a>
+                                            <a className="collapse-item" href="blank.html">Blank Page</a>
+                                        </div>
+                                    </div>
+                                </li>
+
+                                {/* <!-- Nav Item - Charts --> */}
+                                <li className="nav-item">
+                                    <a className="nav-link" href="charts.html">
+                                        <i className="fas fa-fw fa-chart-area"></i>
+                                        <span>Charts</span></a>
+                                </li>
+
+                                {/*  <!-- Nav Item - Tables --> */}
+                                <li className="nav-item">
+                                    <a className="nav-link" href="tables.html">
+                                        <i className="fas fa-fw fa-table"></i>
+                                        <span>Tables</span></a>
+                                </li>
+
+                                {/* <!-- Divider --> */}
+                                <hr className="sidebar-divider d-none d-md-block" />
+                          
+                        </ul>
                         {/*   <!-- Sidebar Toggler (Sidebar) --> */}
                         {/*   <div className="text-center d-none d-md-inline">
                             <button className="rounded-circle border-0" id="sidebarToggle" onClick={changeStyle}></button>
                         </div> */}
 
                         {/*  <!-- Sidebar Message --> */}
-
+                       
 
                     </nav>
                 </div>
